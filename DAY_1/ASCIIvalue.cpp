cda2686167efe9633954162c9fd86c0deec99ec3
nawazashraf <ashrafnawaz843@gmail.com>
2025-08-27 19:28:32 +0530

    C++ Basics

---
+++ DAY_1/ASCIIvalue.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << (int) 'a' << endl; 
+    cout << (char) ('a' + 1) << endl; 
+}
---
+++ DAY_1/helloWorld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<< "Hello World" << endl;
+
+    ///printing something
+    cout << "Hello";
+    //Printing multiple values in same line
+
+    cout << "Helo " << "World" << "123" << 5;
+
+    //printing on new Line
+    cout << "Howww \n" << "howwwww";
+    cout << "Howww" << endl;
+    cout << "howwwwww";
+}
