ae5fbbc27a8e7dfb2788d6165a2d454b07c2ef4b
nawazashraf <ashrafnawaz843@gmail.com>
2025-09-04 11:23:42 +0530

    update

---
+++ Sheet-1(DataTypeNcondition)/Q3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long a , b;
+
+    cin >> a >> b;
+
+    cout << a%10 + b%10;
+
+}
+
+
+
+
+
+/*
+
+Given two numbers N and M. Print the summation of their last digits.
+
+Input
+Only one line containing two numbers N, M (0 ≤ N, M ≤ 1018).
+
+Output
+Print the answer of the problem.
+
+Example
+InputCopy
+13 12
+OutputCopy
+5
+
+
+https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+*/
